545f747fea9f8a083f41573f5d139ba4f2a6ecf3
QingYuMars <wechat987654321@163.com>
2020-02-24 15:18:13 +0800

    index page

---
+++ src/main/java/life/majiang/community/dto/PaginationDTO.java
@@ -0,0 +1,18 @@
+package life.majiang.community.dto;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class PaginationDTO<T> {
+    private List<T> data;
+    private boolean showPrevious;
+    private boolean showFirstPage;
+    private boolean showNext;
+    private boolean showEndPage;
+    private Integer page;
+    private List<Integer> pages = new ArrayList<>();
+    private Integer totalPage;
+}
