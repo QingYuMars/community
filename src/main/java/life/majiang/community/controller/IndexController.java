a6c15e061ee346743d00cb4e56a77f4c1a9443c9
QingYuMars <wechat987654321@163.com>
2020-02-25 16:02:32 +0800

    complete

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -1,15 +1,19 @@
 package life.majiang.community.controller;
 
+import life.majiang.community.cache.HotTagCache;
+import life.majiang.community.dto.PaginationDTO;
 import life.majiang.community.dto.QuestionDTO;
 import life.majiang.community.mapper.QuestionMapper;
 import life.majiang.community.mapper.UserMapper;
 import life.majiang.community.model.Question;
 import life.majiang.community.model.User;
 import life.majiang.community.service.QuestionService;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -22,9 +26,30 @@ public class IndexController {
     @Autowired
     private QuestionService questionService;
 
-    @GetMapping("/index")
+    @Autowired
+    private HotTagCache hotTagCache;
+
+    @GetMapping("/")
+    public String index(Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "10") Integer size,
+                        @RequestParam(name = "search", required = false) String search,
+                        @RequestParam(name = "tag", required = false) String tag,
+                        @RequestParam(name = "sort", required = false) String sort) {
+        PaginationDTO pagination = questionService.list(search, tag, sort, page, size);
+        List<String> tags = hotTagCache.getHots();
+        model.addAttribute("pagination", pagination);
+        model.addAttribute("search", search);
+        model.addAttribute("tag", tag);
+        model.addAttribute("tags", tags);
+        model.addAttribute("sort", sort);
+        return "index";
+    }
+    /*@GetMapping("/index")
     public String index(HttpServletRequest request,
-                        Model model){
+                        Model model,
+                        @RequestParam(name = "page",defaultValue = "1") Integer page,
+                        @RequestParam(name = "size",defaultValue = "5") Integer size){
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
@@ -39,8 +64,9 @@ public class IndexController {
             }
         }
         //查询数据
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions",questionList);
+        PaginationDTO pagination = questionService.list(page,size);
+        //PaginationDTO pagination = questionService.list(search, tag, sort, page, size);
+        model.addAttribute("pagination",pagination);
         return "index";
-    }
+    }*/
 }
---
+++ src/main/java/life/majiang/community/enums/SortEnum.java
@@ -0,0 +1,11 @@
+package life.majiang.community.enums;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
+public enum SortEnum {
+    HOT,
+    HOT30,
+    HOT7,
+    NO,
+    NEW;
+}
